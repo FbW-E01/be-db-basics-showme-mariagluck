10d05b047f6647f7fdc73b9291b0a53b534e6e16
mariagluck <mariamart.berlin@gmail.com>
2021-09-15 14:55:58 +0200

    more experiments

--- database/database.sql
+++ database/database.sql
@@ -5,15 +5,17 @@ SHOWDATABASES;
 USE addPoems;
 
 CREATE TABLE PoemUsers(
+    id INT NOT NULL AUTO_INCREMENT,
     pseudoname varchar(100),
     title varchar(1000) not null,
-    email TEXT(255) not null, 
+    abstract TEXT(500) not null, 
     createdby TEXT(255) not null,
     location TEXT(100),
     date DATETIME,
     wordcount INT(255) not null,
     rating INT(255) not null,
-    published BOOLEAN NOT NULL
+    published BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
 );
 
 
@@ -35,14 +37,14 @@ ALTER TABLE PoemUsers
 -- TESTING INSERT INTO
 INSERT INTO PoemUsers(type) VALUES('date', CURRENT_DATE());
 
-
 -- this doesnt work to modify the type from varchar to current date
 -- ALTER TABLE PoemUsers
 --   MODIFY date CURRENT_DATE() NOT NULL;
 
 -- THis doesnt work
-INSERT INTO PoemUsers(title, published)
-   VALUES('potato Poem', 1);
+INSERT INTO PoemUsers
+     title
+   VALUES 'potato Poem';
 
 
 SELECT pseudoname
@@ -56,11 +58,51 @@ DESCRIBE PoemUsers;
 
 -- here I create a new table with a different logic to test
 CREATE TABLE Poems (
-title varchar(20) NOT NULL,
-poem TEXT(10000) NOT NULL,
-date DATE,
-time TIME
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(20) NOT NULL,
+    abstract text(500) NOT NULL,
+    poem TEXT(10000) NOT NULL,
+    date DATETIME,
+    published BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
 );  
+
+
+INSERT INTO 
+    Poems(title, abstract, poem, date, published)
+VALUES
+    ("Potatoem", "I like trains. They are Great. But not as great as a potato.", "He was traveling from Chicago
+to Joliet, he said, on the expressway,
+Old State Highway 59, when a
+semi rollover caused a load of potatoes
+to scatter across the road.
+
+People stopped, pulled their
+pickups and jeeps, their Chevy vans
+and VW bugs off to the shoulder,
+got out and dashed across three lanes
+of traffic after Idaho russets and
+Yukon Golds, reds and whites and yams.
+
+I’d have understood if it were
+a Brinks truck with flyaway tens
+and twenties. But potatoes?
+Perhaps it was the fact of
+sudden bounty dropping down
+in front of you, and like unexpected
+grace, you must be grateful,
+whatever it is that is given.", "2021-01-01T13:37:00", 1);
+
+
+SELECT title, abstract FROM Poems;
+
+SELECT title, abstract FROM Poems WHERE published = 0;
+
+UPDATE Poems SET abstract = "Haters gonna hate, potatoes gonna potate" WHERE published = 0;
+
+DELETE FROM poems WHERE id=1;
+
+SELECT id FROM Poems WHERE published = 0;
 
 -- here I delete the table
 DROP TABLE Poems;
